069161cff50a7bd02da031f5120cdcf0e2d86656
Samuel Gordalina <samuel.gordalina@gmail.com>
2023-12-12 14:10:11 -1000

    Add hide flag to Todos

--- src/app/api/todos/route.js
+++ src/app/api/todos/route.js
@@ -3,5 +3,11 @@ const { prisma } = await import("@/lib/prisma");
 export const dynamic = "force-dynamic";
 
 export async function GET(_request) {
-  return Response.json(await prisma.todo.findMany());
+  const todos = await prisma.todo.findMany({
+    where: {
+      hide: false,
+    },
+  });
+
+  return Response.json(todos);
 }
